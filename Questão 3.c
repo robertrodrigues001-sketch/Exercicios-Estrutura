8c7ae885beee90b84e604c3111df6463b056f6b8
robertrodrigues001-sketch <robertrodrigues001@gmail.com>
2026-03-17 08:39:45 -0300

    Create Questão 3.c

---
+++ Questão 3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2, soma;
+
+
+    printf("Digite o primeiro número: ");
+    scanf("%d", &num1);
+
+    printf("Digite o segundo número: ");
+    scanf("%d", &num2);
+
+
+    soma = num1 + num2;
+
+    
+    printf("A soma é: %d\n", soma);
+
+    return 0;
+}
