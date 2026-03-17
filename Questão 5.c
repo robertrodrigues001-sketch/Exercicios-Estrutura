5628188e40ad967f421e33e0782cf53106474efe
robertrodrigues001-sketch <robertrodrigues001@gmail.com>
2026-03-17 08:44:43 -0300

    Create Questão 5.c

---
+++ Questão 5.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int num;
+
+    
+    printf("Digite um número: ");
+    scanf("%d", &num);
+
+    
+    printf("O sucessor de %d é %d\n", num, num + 1);
+
+    return 0;
+}
